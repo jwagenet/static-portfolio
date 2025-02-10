dc4f19ac2f0f0d72331edf75d2aa18487ef3d635
Jonathan Wagenet <jonathanwagenet@gmail.com>
2025-02-10 12:05:20 -0500

    index up and running

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import img from "$lib/images/index.jpg";
+</script>
+
+
+<section>
+    <!--<p>Projects</p>-->
+    <p>I'm a mechatronics engineer interested in robotics, 3D printing, and product design.</p>
+    <p>I complement my mechanical training with electronics and programming experience.</p>
+    <p>Find me on <a href="https://github.com/jwagenet">Github</a>, <a href="https://www.linkedin.com/in/jonathanwagenet">LinkedIn</a></p>
+</section>
+
+<section class="image">
+    <img style="width:700px;height:auto;" src={img} />
+</section>
