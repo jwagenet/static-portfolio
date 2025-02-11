bc94c60c2799969fa3088f00d2985d833c06d95a
Jonathan Wagenet <jonathanwagenet@gmail.com>
2025-02-11 16:04:33 -0500

    add doodles to index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,16 @@
 <section>
     <p>I'm a mechatronics engineer interested in robotics, 3D printing, and product design.</p>
     <p>I complement my mechanical training with electronics and programming experience.</p>
-    <p>Find me on <a href="https://github.com/jwagenet">Github</a>, <a href="https://www.linkedin.com/in/jonathanwagenet">LinkedIn</a></p>
+    <br />
+    <ul>Find me on
+        <li><a href="https://github.com/jwagenet">Github</a></li>
+        <li><a href="https://www.linkedin.com/in/jonathanwagenet">LinkedIn</a></li>
+    </ul>
+    <br />
+    <h2>Doodles</h2>
+    <ul>
+        <li><a href="/puzzle-stats/">NYT Mini Stats</a></li>
+    </ul>
 </section>
 
 <section class="image">
